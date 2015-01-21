1c11bfb94ccfd113e4bd19e9b4916c17151b2ef0
Cobbleopolis <agounimon@yahoo.com>
2015-01-20 22:03:44 -0500

    ADDED ALL THE 3D! HAHAHAHAHAHAHAHA

--- core/src/com/cobbleopolis/scape/Scape.java
+++ core/src/com/cobbleopolis/scape/Scape.java
@@ -1,31 +1,95 @@
 package com.cobbleopolis.scape;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
-public class Scape extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	int x = 0;
-	int y = 0;
-	
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Scape implements ApplicationListener {
+	public PerspectiveCamera cam;
+	public ModelBatch modelBatch;
+	public Model model;
+	public Environment environment;
+	public CameraInputController camController;
+
+	public List<ModelInstance> models = new ArrayList<ModelInstance>();
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+		modelBatch = new ModelBatch();
+
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
+
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(5f, 5f, 5f,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+				Usage.Position | Usage.Normal);
+		addModel(model, 0f, 0f ,0f);
+		model = modelBuilder.createSphere(5.5f, 5.5f, 5.5f, 32, 32, new Material(ColorAttribute.createDiffuse(Color.BLUE)), Usage.Position | Usage.Normal);
+		addModel(model, 6f, 0f, 0f);
+
+
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, x, y);
-		batch.end();
-		x++;
-		y++;
+	public void render() {
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		camController.update();
+
+		modelBatch.begin(cam);
+		for(int i = 0; i < models.size(); i++) {
+			modelBatch.render(models.get(i), environment);
+		}
+		modelBatch.end();
+	}
+
+	@Override
+	public void dispose() {
+		modelBatch.dispose();
+		model.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	public void addModel(Model model, float x, float y, float z){
+		ModelInstance instance;
+		instance = new ModelInstance(model);
+		instance.transform.translate(x, y, z);
+		models.add(instance);
 	}
 }
